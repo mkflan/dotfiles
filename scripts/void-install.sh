3545d64cad512bdea7cf4a2fa9dd532fede5f7ce
mkflan <dev.upstage256@slmails.com>
2024-11-23 08:53:16 -0600

    changes

---
+++ scripts/void-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+exec_quiet () {
+  $1 >/dev/null
+}
+
+exec_quiet "sudo xbps-install -Sy"
+
+# Install all programs
+exec_quiet "sudo xbps-install -y git stow delta alacritty keepassxc bat eza tmux zoxide helix ripgrep syncthing newsboat flatpak qemu gcc"
+
+# Install Brave Browser, Spotify (maybe get spotify-tui?), Obsidian, Discord, Todoist using flatpak
+exec_quiet "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo"
+
+# Clone dotfiles
+exec_quiet "git clone git@github.com:mkflan/dotfiles.git $HOME/.dotfiles"
+rm ~/.bashrc && cd .dotfiles && stow --adopt . && cd - 
+
+# Clone TPM
+exec_quiet "git clone https://github.com/tmux-plugins/tpm $HOME/plugins/tpm"
