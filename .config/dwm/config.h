f84ffe80c1a268ad8ad26c2d1e82fe451f558bd0
mkflan <dev.upstage256@slmails.com>
2025-05-31 15:56:19 -0500

    working on dwm config

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -6,18 +6,17 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 40;
+static const int user_bh            = 60;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_text[]        = "#bbbbbb";
+static const char norm_bg[]         = "#222222";
+static const char sel_bg[]          = "#000000";
+static const char col_border[]      = "#444444";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_text, norm_bg, col_border },
+	[SchemeSel] = { col_text, sel_bg, col_border },
 };
 
 /* tagging */
@@ -58,8 +57,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "j4-dmenu-desktop", NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -86,6 +87,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,                            XK_Print,  spawn,          {.v = screenshotcmd } },
+	{ 0,                            XF86XK_MonBrightnessUp,    spawn,           SHCMD("brightnessctl s +5%")},
+	{ 0,                            XF86XK_MonBrightnessDown,  spawn,           SHCMD("brightnessctl s 5%-")},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
--- .config/dwmblocks/blocks.h
+++ .config/dwmblocks/blocks.h
@@ -1,9 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"⚡ ", "~/dotfiles/scripts/statusbar/battery.sh",					5,		0},
+	{"", "date '+%d/%m/%Y %T  '",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
